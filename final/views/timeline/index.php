620407d8ee2c93082b3b44087482941d29434ff7
philippklemmer <philipp@Philipps-MBP.lan>
2016-07-20 18:48:24 +0200

    Structure: timline cleanUp and Placeholders to get the correct style, Style: Styled the timelinepeaces so if going to focus on datathings i don't need to do the style anymore

--- final/views/timeline/index.php
+++ final/views/timeline/index.php
@@ -1,24 +1,59 @@
 <!--MAIN CONTEN -> Post on Timeline-->
 <div id="timelineWrapper">
     <section class="">
-        <div class="contentHeader">
+        <div class="postHeader">
             <div class="profilImg"></div>
-            <h4>Philipp Klemmer</h4>
-            <span class="seperator"></span>
-            <span class="timeAgo">14min</span>
-            <p class="comment">Smooth raw run from teamrider @pieter.daenen 👌 Hope you all are
-enjoying the lovely weather as we are.</p>
+            <div class="postTextContainer">
+                <h4>Philipp Klemmer</h4>
+                <span class="circleSeperator"></span>
+                <span class="timeAgo">14min</span>
+                <p class="comment">
+                    Smooth raw run from teamrider @pieter.daenen 👌 Hope you all are enjoying the lovely weather as we are.
+                </p>
+            </div>
         </div>
-        <div class="mainContent">
+        <div class="postMain">
             <!--Video oder Image-->
             <!--Check if Video or Image-->
             <div class="vidOrImg"></div>
             <div class="likeCountContainer">
                 <span class="heart"></span>
-                <p class="likeCount">123123</p>
+                <p class="likeCount">Like</p>
             </div>
             <!--Foreach for the tags-->
-            <div class="postTags"><p>#longboard, #star, #dancing    </p></div>
+            <div class="postTags">
+                <a href="#"><span>#</span>longboard</a>,
+                <a href="#"><span>#</span>star</a>,
+                <a href="#"><span>#</span>dancing</a>,
+            </div>
+        </div>
+    </section >
+    <section class="">
+        <div class="postHeader">
+            <div class="profilImg"></div>
+            <div class="postTextContainer">
+                <h4>Philipp Klemmer</h4>
+                <span class="circleSeperator"></span>
+                <span class="timeAgo">14min</span>
+                <p class="comment">
+                    Smooth raw run from teamrider @pieter.daenen 👌 Hope you all are enjoying the lovely weather as we are.
+                </p>
+            </div>
+        </div>
+        <div class="postMain">
+            <!--Video oder Image-->
+            <!--Check if Video or Image-->
+            <div class="vidOrImg"></div>
+            <div class="likeCountContainer">
+                <span class="heart"></span>
+                <p class="likeCount">Like</p>
+            </div>
+            <!--Foreach for the tags-->
+            <div class="postTags">
+                <a href="#"><span>#</span>longboard</a>,
+                <a href="#"><span>#</span>star</a>,
+                <a href="#"><span>#</span>dancing</a>,
+            </div>
         </div>
     </section >
 
@@ -27,16 +62,12 @@ enjoying the lovely weather as we are.</p>
     <!--Swipe from left left to right or right to left-->
         <div class="menuContainer">
             <div class="menuHeader">
-                <img class="profileImg" src="" alt="" />
-                <p class="username"></p>
+                <img class="profileImg" src="/public/img/testThumnail.png" alt="" />
+                <p class="username">Philipp Klemmer</p>
             </div>
             <div class="menuMain">
-
-                    <span class="userprofileIcon"></span>
-                    <p><a href="#">UserProfile</a></p>
-
-                    <span class="editProfileIcon"></span>
-                    <p><a href="#">UserBackend</a></p>
+                    <p><a href="#"><span class="userprofileIcon"></span>UserProfile</a></p>
+                    <p><a href="#"><span class="editProfileIcon"></span>UserBackend</a></p>
             </div>
             <a href="#">Logout</a>
         </div>
